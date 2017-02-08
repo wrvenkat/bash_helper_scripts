33bcb68f473ce5055b454c8db222b91f9d3a1ec2
W.R.Venkat <wrvenkat@gmail.com>
2017-02-07 22:18:55 -0500

    Added usage message.

--- logger.sh
+++ logger.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#possbile scopes,
-#0 - make colour optional
-#1 - make logger invokeable and sourceable
-#2 - replace function arguments with command-line arguments
-#3 - support logall - log both INFO and ERROR messages to file
-#4 - support custom file option replacing default file
-
 LOG_SOURCE_FLG=0
 LOG_FIRST_RUN=0
 
